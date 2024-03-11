ac1929723ca66e9a1477d6ed0d64d704030c6014
bondarenkoai <114067419+bondarenkoai@users.noreply.github.com>
2024-03-11 15:34:43 +0200

    Profile.jsx - Done

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -4,16 +4,19 @@ import users from '../../data/users.json';
 import { Stats } from './Stats/Stats';
 
 export const Profile = () => {
-    const user = users[0];
     return (
-        <div className={css.profile}>
-            <Description
-                username={user.username}
-                tag={user.tag}
-                location={user.location}
-                avatar={user.avatar}
-            />
-            <Stats stats={user.stats} />
+        <div className={css.profilePosition}>
+            {users.map(user => (
+                <div className={css.profile}>
+                    <Description
+                        username={user.username}
+                        tag={user.tag}
+                        location={user.location}
+                        avatar={user.avatar}
+                    />
+                    <Stats stats={user.stats} />
+                </div>
+            ))}
         </div>
     );
 };
--- src/Components/Profile/Stats/Stats.jsx
+++ src/Components/Profile/Stats/Stats.jsx
@@ -3,18 +3,18 @@ import css from './Stats.module.css';
 export const Stats = ({ stats: { followers, views, likes } }) => {
     return (
         <div className={css.stats}>
-            <ul className="stats">
-                <li>
-                    <span className="label">Followers</span>
-                    <span className="quantity">{followers}</span>
+            <ul className={css.statsList}>
+                <li className={css.statsItem}>
+                    <span className={css.label}>Followers</span>
+                    <span className={css.quantity}>{followers}</span>
                 </li>
-                <li>
-                    <span className="label">Views</span>
-                    <span className="quantity">{views}</span>
+                <li className={css.statsItem}>
+                    <span className={css.label}>Views</span>
+                    <span className={css.quantity}>{views}</span>
                 </li>
-                <li>
-                    <span className="label">Likes</span>
-                    <span className="quantity">{likes}</span>
+                <li className={css.statsItem}>
+                    <span className={css.label}>Likes</span>
+                    <span className={css.quantity}>{likes}</span>
                 </li>
             </ul>
         </div>
