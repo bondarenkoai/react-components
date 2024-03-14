a1de257fcee27df3e9fefa1224e10fc1352cb6b4
bondarenkoai <114067419+bondarenkoai@users.noreply.github.com>
2024-03-14 23:49:59 +0200

    WorkSticker - Done

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,7 +1,7 @@
 import css from './App.module.css';
 import { Friends } from './Friends/Friends';
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
+// import { Profile } from './Profile/Profile';
+// import { Statistics } from './Statistics/Statistics';
 
 export default function App() {
     return (
